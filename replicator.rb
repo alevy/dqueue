402637b5b87a569dd2a7c8caaf19e38a847fc958
Amit Levy <levya@marykate.cs.washington.edu>
2009-12-03 15:29:31 -0800

    merge

--- replicator.rb
+++ replicator.rb
@@ -1,14 +1,16 @@
 module DQueue
-  class Replicator
+ class Replicator
+
+  attr_reader :rep_thresh
   
-    def initialize(master)
-        @data_to_nodes = Hash.new
-        @nodes_to_data = Hash.new
-        @master = master
-        @heartbeats = Hash.new
-        Thread.abort_on_exception = true
-        Thread.new{while true do sleep 10; check_heartbeats; end}
-    end
+  def initialize(master)
+      @data_to_nodes = Hash.new
+      @nodes_to_data = Hash.new
+      @master = master
+      @heartbeats = Hash.new
+      Thread.abort_on_exception = true
+      Thread.new{while true do sleep 10; check_heartbeats; end}
+  end
   
     def get_heartbeat(node)
       @heartbeats[node] = Time.now
@@ -16,33 +18,47 @@ module DQueue
     
     def check_heartbeats
       puts "checking heartbeats..."
-      @heartbeats.each do |key, value|
+      @heartbeats.each do |node, value|
         if Time.now - value > 10
           puts "replicating node!"
-          replicate_node(key)
-          @heartbeats.delete(key)
+          @master.remove_node(node)
+          
+          data = get_data_list(node)
+          data.each do |element|
+            @data_to_nodes[element].delete(node)
+          end
+          @nodes_to_data.delete(node)
+           
+          replicate_node(node)
+          @heartbeats.delete(node)
           puts "successfully replicated node"
         end
       end
     end
-     
-    #replicate the given item ID on num additional nodes
+    
     def replicate(item_id, num)
       num.times do
-        replicate_once(item_id)
-      end
+        replicate_once(item_id)      end
     end
-    
-    #replicate the given item ID on num additional nodes
+     
+    #replicate the given item ID on an additional node
     def replicate_once(item_id)
-      current_nodes = @data_to_nodes[item_id]
+      current_nodes = find_nodes(item_id)
       target_node = next_replica
       while(current_nodes.include?(target_node)) do
         target_node = next_replica
       end
       
       target_node.add_data(item_id, current_nodes[0].get_data(item_id))
+      
+      #TODO if the above line failed, re-start this method.
       add_replica(item_id, target_node)
+    end
+    
+   #find all nodes this item ID is currently stored on
+    def find_nodes(item_id)
+      return @data_to_nodes[item_id]
+      
     end
     
     #choose the next node to replicate on
@@ -60,8 +76,11 @@ module DQueue
 
     #get the list of data elements stored on this node
     def get_data_list(node_id)
-      return @nodes_to_data[node_id]
-      
+      if @nodes_to_data[node_id].nil?
+        return []
+      else
+        return @nodes_to_data[node_id]
+      end
     end
 
     #mark this item as stored at this node
@@ -82,7 +101,7 @@ module DQueue
     #remove the metadata info about this item, it's no longer
     #needed.
     def clear_replicas(item_id)
-      nodes = @data_to_nodes[item_id]
+      nodes = find_nodes(item_id)
       nodes.each do |node|
         @nodes_to_data[node].delete(item_id)
       end
